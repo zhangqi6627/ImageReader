59b6fd5645a2983fdf662f5901681819e895a8f8
zhangqi <zhangqi@sagereal.com>
2024-06-28 17:39:17 +0800

    init ImageReader
    Change-Id: Ib2c0b2798db3e33581ecd230ac0a7df487c6f59e

--- app/src/main/java/com/james/imagereader/AlbumsActivity.java
+++ app/src/main/java/com/james/imagereader/AlbumsActivity.java
@@ -1,15 +1,21 @@
 package com.james.imagereader;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageInstaller;
 import android.content.res.Resources;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.james.imagereader.R;
@@ -17,6 +23,7 @@ import com.james.imagereader.R;
 import java.util.List;
 
 public class AlbumsActivity extends BaseActivity {
+    private final static String TAG = "AlbumsActivity";
     private final Context mContext = AlbumsActivity.this;
     private RecyclerView rv_albums;
     private AlbumsAdapter albumsAdapter;
@@ -25,9 +32,6 @@ public class AlbumsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_albums);
-        TextView tv_title = getWindow().getDecorView().findViewById(android.R.id.title);
-        tv_title.setText("aaa");
-        setTitle("hall");
         rv_albums = findViewById(R.id.rv_albums);
         albumsAdapter = new AlbumsAdapter(mContext, getInstalledPackages());
         rv_albums.setAdapter(albumsAdapter);
@@ -36,11 +40,21 @@ public class AlbumsActivity extends BaseActivity {
         rv_albums.setLayoutManager(layoutManager);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        albumsAdapter.updateAlbums(getInstalledPackages());
+        rv_albums.setAdapter(albumsAdapter);
+    }
+
     class AlbumsAdapter extends RecyclerView.Adapter<AlbumsHolder> {
-        private final List<String> mAlbums;
+        private List<String> mAlbums;
         private final Context mContext;
         public AlbumsAdapter(Context context, List<String> albums) {
             mContext = context;
+            mAlbums = albums;
+        }
+        public void updateAlbums(List<String> albums) {
             mAlbums = albums;
         }
         @NonNull
@@ -65,6 +79,14 @@ public class AlbumsActivity extends BaseActivity {
             });
             int progress = loadData(packageName);
             holder.tv_progress.setText(""+progress);
+            holder.ll_content.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Log.e(TAG, "packageName : " + packageName);
+                    uninstall(packageName);
+                    return true;
+                }
+            });
         }
 
         @Override
@@ -72,21 +94,30 @@ public class AlbumsActivity extends BaseActivity {
             return mAlbums.size();
         }
     }
+    private void uninstall(String packageName) {
+        Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent sender = PendingIntent.getActivity(mContext, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        PackageInstaller mPackageInstaller = getPackageManager().getPackageInstaller();
+        mPackageInstaller.uninstall(packageName, sender.getIntentSender());// 卸载APK
+    }
 
     static class AlbumsHolder extends RecyclerView.ViewHolder {
+        LinearLayout ll_content;
         ImageView iv_album;
         TextView tv_title;
         TextView tv_progress;
         public AlbumsHolder(@NonNull View itemView) {
             super(itemView);
+            ll_content = itemView.findViewById(R.id.ll_content);
             iv_album = itemView.findViewById(R.id.iv_album);
             tv_title = itemView.findViewById(R.id.tv_title);
             tv_progress = itemView.findViewById(R.id.tv_progress);
         }
     }
-    private String getAssetString(String packageName, String strName) {
+    private String getAssetString(String packageName, String idName) {
         Resources mResources = loadPackageResource(packageName);
-        int strId = mResources.getIdentifier(strName, "string", packageName);
+        int strId = mResources.getIdentifier(idName, "string", packageName);
         return mResources.getString(strId);
     }
 }
